af6243cddd914dd4a69fb9a76d639969a6d9a9e1
Russell <russell.tarpley@gmail.com>
2016-12-11 18:45:51 -0600

    chore(Initial Build)
    Working on distribution placement

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,73 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  var gulp = require('gulp'),
-      concat = require('gulp-concat'),
-      rename = require('gulp-rename'),
-      sass = require('gulp-sass'),
-      sourcemaps = require('gulp-sourcemaps'),
-      sassFiles = [
-        './src/global.scss',
-        './src/**/*.scss'
-      ],
-      pump = require('pump'),
-      uglify = require('gulp-uglify'),
-      jsFiles = ['src/**/*.js'];
+    var gulp = require('gulp'),
+        concat = require('gulp-concat'),
+        rename = require('gulp-rename'),
+        sass = require('gulp-sass'),
+        sourcemaps = require('gulp-sourcemaps'),
+        sassFiles = [
+          './src/global.scss',
+          './src/**/*.scss'
+        ],
+        uglify = require('gulp-uglify'),
+        jsFiles = ['src/**/*.js'];
 
-  gulp.task('sass-compress', function() {
-    gulp
-      .src(sassFiles)
-      .pipe(concat('file.scss'))
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-        outputStyle: 'compressed'
-      }).on('error', sass.logError))
-      .pipe(rename(function(path) {
-        path.dirname = '';
-        path.basename = 'mdl-date-textfield';
-        path.extname = '.min.css';
-      }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./dist'));
-  });
+    gulp.task('sass-compress', function () {
+        gulp
+          .src(sassFiles)
+          .pipe(concat('file.scss'))
+          .pipe(sourcemaps.init())
+          .pipe(sass({
+              outputStyle: 'compressed'
+          }).on('error', sass.logError))
+          .pipe(rename(function (path) {
+              path.dirname = '';
+              path.basename = 'mdl-date-textfield';
+              path.extname = '.min.css';
+          }))
+          .pipe(sourcemaps.write('./'))
+          .pipe(gulp.dest('./dist'));
+    });
 
-  gulp.task('sass', function () {
-    gulp
-      .src(sassFiles)
-      .pipe(concat('mdl-date-textfield.scss'))
-      .pipe(sass({
-        sourceComments: false
-      }).on('error', sass.logError))
-      .pipe(gulp.dest('./dist'));
-  });
+    gulp.task('sass', function () {
+        gulp
+          .src(sassFiles)
+          .pipe(concat('mdl-date-textfield.scss'))
+          .pipe(sass({
+              sourceComments: false
+          }).on('error', sass.logError))
+          .pipe(gulp.dest('./dist'));
+    });
 
-  gulp.task('js-compress', function() {
-    gulp
-      .src(jsFiles)
-      .pipe(uglify('mdl-date-textfield.min.js', {
-        outSourceMap: true
-      }))
-      .pipe(gulp.dest('./dist'));
-  });
+    gulp.task('js-compress', function () {
+        gulp
+          .src(jsFiles)
+          .pipe(uglify('mdl-date-textfield.min.js', {
+              outSourceMap: true
+          }))
+          .pipe(gulp.dest('./dist'));
+    });
 
-  gulp.task('js', function() {
-    gulp
-      .src(jsFiles)
-      .pipe(rename(function(path) {
-        path.dirname = '';
-        path.basename = 'mdl-date-textfield';
-        path.extname = '.js';
-      }))
-      .pipe(gulp.dest('./dist'));
-  });
+    gulp.task('js', function () {
+        gulp
+          .src(jsFiles)
+          .pipe(rename(function (path) {
+              path.dirname = '';
+              path.basename = 'mdl-date-textfield';
+              path.extname = '.js';
+          }))
+          .pipe(gulp.dest('./dist'));
+    });
 
-  gulp.task('watch', function () {
-    gulp.watch(sassFiles, ['sass', 'sass-compress']);
-    gulp.watch(jsFiles, ['js', 'js-compress']);
-  });
+    gulp.task('watch', function () {
+        gulp.watch(sassFiles, ['sass', 'sass-compress']);
+        gulp.watch(jsFiles, ['js', 'js-compress']);
+    });
 
-  gulp.task('build', ['sass', 'sass-compress', 'js', 'js-compress']);
+    gulp.task('build', ['sass', 'sass-compress', 'js', 'js-compress']);
 
-  // The default task (called when you run `gulp` from cli)
-  gulp.task('default', ['watch']);
+    // The default task (called when you run `gulp` from cli)
+    gulp.task('default', ['watch']);
 
 })();
